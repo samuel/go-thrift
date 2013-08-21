92e63c49adf450d4c1c89fde16bd06db954ac65f
Samuel Stauffer <samuel@descolada.com>
2013-08-21 16:18:29 -0700

    Add helper functions to create pointer values

---
+++ thrift/helpers.go
@@ -0,0 +1,25 @@
+package thrift
+
+func Bool(v bool) *bool {
+	return &v
+}
+
+func Float32(v float32) *float32 {
+	return &v
+}
+
+func Float64(v float64) *float64 {
+	return &v
+}
+
+func Int32(v int32) *int32 {
+	return &v
+}
+
+func Int64(v int64) *int64 {
+	return &v
+}
+
+func String(v string) *string {
+	return &v
+}
