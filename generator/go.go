38fdb75d5194540195b62ca44253923cdb90efea
David Siklosi <david.siklosi@prezi.com>
2016-01-19 15:21:53 +0100

    Fixing dependencies

--- generator/go.go
+++ generator/go.go
@@ -19,7 +19,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/samuel/go-thrift/parser"
+	"github.com/siklosid/go-thrift/parser"
 )
 
 var (
--- generator/main.go
+++ generator/main.go
@@ -13,8 +13,8 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/samuel/go-thrift/parser"
-	"github.com/samuel/go-thrift/thrift"
+	"github.com/siklosid/go-thrift/parser"
+	"github.com/siklosid/go-thrift/thrift"
 )
 
 func camelCase(st string) string {
